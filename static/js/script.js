3eff5b4bfb026c341d57c2b35e2c8f5d6e781088
Sloth-iNkt <ciavelm18@gmail.com>
2025-03-04 18:56:09 +0800

    navbar mobile responsiveness

--- static/js/script.js
+++ static/js/script.js
@@ -21,3 +21,25 @@ function prevStep(current) {
     document.getElementById(`step${current}`).classList.remove("active");
     document.getElementById(`step${current - 1}`).classList.add("active");
 }
+
+function toggleSidebar() {
+    const navbar = document.getElementById('navbar');
+    const openButton = document.getElementById('open-sidebar');
+    const closeButton = document.getElementById('close-sidebar');
+
+    navbar.classList.toggle('show');
+    openButton.style.display = navbar.classList.contains('show') ? 'none' : 'block';
+    closeButton.style.display = navbar.classList.contains('show') ? 'block' : 'none';
+}
+
+window.addEventListener('resize', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.innerWidth > 700) {
+        navbar.classList.remove('show');
+        document.getElementById('open-sidebar').style.display = 'none';
+        document.getElementById('close-sidebar').style.display = 'none';
+    }else {
+        document.getElementById('open-sidebar').style.display = 'block';
+        document.getElementById('close-sidebar').style.display = 'none';
+    }
+});
